e1d4d18b592bbfd6351cc169f6b0d565adebe453
taufik <taufik@kalsym.com>
2022-06-17 16:04:24 +0800

    bug fix

--- src/main/java/com/kalsym/whatsapp/service/controller/PushTemplateMessageController.java
+++ src/main/java/com/kalsym/whatsapp/service/controller/PushTemplateMessageController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.criteria.Predicate;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.text.ParseException;
